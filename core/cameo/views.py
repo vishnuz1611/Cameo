4f29ba83a3c0b8f90e23374476d4b98a75fa0292
vishnuz1611@gmail.com <vishnuz1611@gmail.com>
2021-12-13 07:05:18 -0500

    Added Search

--- core/cameo/views.py
+++ core/cameo/views.py
@@ -1,10 +1,20 @@
+from django.http import response
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+import requests
 
-# from django.http import HttpResponse
-# Create your views here.
 
 def index(request):
+    query = request.GET.get('q')
+    if query:
+        url = 'http://www.omdbapi.com/?apikey=64830d56&s=' + query
+        response = requests.get(url)
+        data = response.json()
+        context = {
+            'query' : query,
+            'data' : data,
+        }
+        return render(request, 'results.html', context)      
     return render(request, 'index.html', context={})
 
 def register(request):
